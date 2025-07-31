817e660d705afd50f6d74517c09a9b55394715ad
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-07-31 02:16:09 -0300

    Atualização do caminho para o arquivo "leis.json"!

--- verificar_qtd_leis.py
+++ verificar_qtd_leis.py
@@ -1,6 +1,6 @@
 import json
 
-with open("tests/leis_100.json", encoding="utf-8") as f:
+with open("tests/leis.json", encoding="utf-8") as f:
     leis = json.load(f)
 
 print(f"Quantidade de leis no arquivo: {len(leis)}")
