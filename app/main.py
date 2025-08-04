04a0d890a0133611662d54e156ebf5e349ad67e7
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-08-04 17:54:09 -0300

    Adição do caminho para o "index.html"!

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from app.routes import query, importar, consulta
 
 app = FastAPI(title="API Leis Ambientais")
+
+# Servir arquivos estáticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Rota para a página inicial
+@app.get("/")
+async def read_index():
+    return FileResponse('static/index.html')
 
 app.include_router(query.router)
 app.include_router(importar.router)
