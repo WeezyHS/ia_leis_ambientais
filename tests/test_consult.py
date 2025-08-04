656025b097be1e5ea54d1b97cc7ae167519ae374
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-08-04 10:32:22 -0300

    Modificação na exibição das respostas e adição da exibição das leis relacionadas!

--- tests/test_consult.py
+++ tests/test_consult.py
@@ -10,6 +10,9 @@ def test_consulta_endpoint():
     assert response.status_code == 200
     json_data = response.json()
 
-    assert "resultados" in json_data
-    assert isinstance(json_data["resultados"], list)
-    assert len(json_data["resultados"]) > 0
+    assert "resposta" in json_data
+    assert isinstance(json_data["resposta"], str)
+    assert len(json_data["resposta"]) > 0
+    
+    assert "leis_relacionadas" in json_data
+    assert isinstance(json_data["leis_relacionadas"], list)
